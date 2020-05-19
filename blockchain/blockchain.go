643bcec14c97cfada592b6a33e505bc8f8f14c28
Ethan Kim(김경업) <ethan.kim@groundx.xyz>
2020-05-19 14:26:58 +0900

    add error log in state migration

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -358,6 +358,7 @@ func (bc *BlockChain) migrateState(rootHash common.Hash) error {
 		for i := 0; i < len(queue); i++ {
 			result := <-resultCh
 			if result.Err != nil {
+				logger.Error("State migration is failed by resultCh", "result.Err", result.Err)
 				return fmt.Errorf("failed to retrieve node data for %x: %v", result.Hash, result.Err)
 			}
 			results[i] = result
@@ -366,12 +367,14 @@ func (bc *BlockChain) migrateState(rootHash common.Hash) error {
 
 		// Process trie nodes
 		if _, index, err := trieSync.Process(results); err != nil {
+			logger.Error("State migration is failed by process error", "err", err)
 			return fmt.Errorf("failed to process result #%d: %v", index, err)
 		}
 
 		// Commit trie nodes
 		written, writeElapsed, err := bc.stateMigrationCommit(trieSync, targetDB.DiskDB())
 		if err != nil {
+			logger.Error("State migration is failed by commit error", "err", err)
 			return fmt.Errorf("failed to commit data #%d: %v", written, err)
 		}
 
@@ -392,6 +395,7 @@ func (bc *BlockChain) migrateState(rootHash common.Hash) error {
 			// TODO-Klaytn Revert DB.
 			// - copied new DB data to old DB.
 			// - remove new DB
+			logger.Error("State migration is failed by stop")
 			return errors.New("stop state migration")
 		case <-bc.quit:
 			return nil
